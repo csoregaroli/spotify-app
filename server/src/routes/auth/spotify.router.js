b276fd8ab8b5798c11540ec5ac0f6d685435902c
Cameron Soregaroli <csoregaroli@gmail.com>
2023-02-04 15:21:55 -0500

    set up skeleton for oauth

--- server/src/routes/auth/spotify.router.js
+++ server/src/routes/auth/spotify.router.js
@@ -1,11 +1,43 @@
 const express = require('express')
+const passport = require('passport')
+const SpotifyStrategy = require('passport-spotify').Strategy
 
 const spotifyAuthRouter = express.Router()
 
-function test(req, res) {
-  return res.send('This is the spotify auth router')
+const AUTH_OPTIONS = {
+  callbackUrl: '',
+  clientId: '',
+  clientSecret: '',
 }
 
-spotifyAuthRouter.get('/', test)
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log('Spotify profile', profile)
+  done(null, profile)
+}
+
+passport.use(new SpotifyStrategy(AUTH_OPTIONS, verifyCallback))
+
+spotifyAuthRouter.get(
+  '/',
+  passport.authenticate('spotify', {
+    scope: '',
+  })
+)
+
+spotifyAuthRouter.get(
+  '/callback',
+  passport.authenticate('spotify', {
+    failureRedirect: '/failure',
+    successRedirect: '/',
+    session: false,
+  }),
+  (req, res) => {
+    console.log('Spotify responded to us tehe')
+  }
+)
+
+spotifyAuthRouter.get('/failure', (req, res) => {
+  return res.send('Failed to log in 😔')
+})
 
 module.exports = spotifyAuthRouter
