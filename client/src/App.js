bf69c58e5b8f56315215a5f3cf2589de0846b985
Cameron Soregaroli <csoregaroli@gmail.com>
2023-02-05 11:53:24 -0500

    finished get user endpoint logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,12 @@
-function App() {
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+const AuthPages = () => {}
+
+const AppPages = () => {}
+
+const AppRoot = () => {}
+
+const App = () => {
   return <div>Hello world</div>
 }
 
--- server/src/routes/user/user.controller.js
+++ server/src/routes/user/user.controller.js
@@ -2,8 +2,18 @@ const { getUserById } = require('../../models/user.model')
 
 async function httpGetCurrentUser(req, res) {
   const id = req.user?.id
-  const user = await getUserById(id)
-  return res.status(200).json(user.data())
+
+  if (!id) {
+    return res.status(200).json(null)
+  }
+
+  try {
+    const user = await getUserById(id)
+    return res.status(200).json(user.data())
+  } catch (err) {
+    console.log(err)
+    return res.status(400).json({ error: 'Could not find user' })
+  }
 }
 
 module.exports = { httpGetCurrentUser }
