54b2267f932147541735ddef5ec019e0643e3932
Cameron Soregaroli <csoregaroli@gmail.com>
2023-02-05 13:57:55 -0500

    fixed req session cookie issue

--- client/src/api/reads.js
+++ client/src/api/reads.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { url } from '../constants/routes'
 
 export const getCurrentUser = async () => {
-  const user = await axios.get(`${url}/user`)
+  const user = await axios.get(url + '/user', { withCredentials: true })
   console.log('from req', user)
   //   const user = await response.json()
   return user
--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,7 @@ const api = require('./routes/api')
 const app = express()
 
 app.use(helmet())
-app.use(cors({ origin: 'http://localhost:3000' }))
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
 app.use(morgan('combined'))
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '..', 'public')))
