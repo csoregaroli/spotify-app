de360801e298cbf2b1fbb8e00a876c3b12d3130a
Cameron Soregaroli <csoregaroli@gmail.com>
2023-02-26 18:04:17 -0500

    formated currently-playing response

--- server/src/routes/spotify/spotify.controller.js
+++ server/src/routes/spotify/spotify.controller.js
@@ -15,7 +15,32 @@ async function httpGetCurrentTrack(req, res) {
     }
   )
 
-  return res.status(200).json(response.data)
+  if (response.status === 200) {
+    const { data } = response
+    const { item, is_playing } = data
+
+    const trackName = item.name
+    const imageUrl = item.album.images[1].url
+
+    //Get artists from response
+    let artists = []
+    const artistsResponse = data.item.artists
+    artistsResponse.forEach((artist) => {
+      artists.push(artist.name)
+    })
+
+    //Set current track
+    const currentTrack = {
+      artists,
+      trackName,
+      imageUrl,
+      isPlaying: is_playing,
+    }
+
+    return res.status(200).json(currentTrack)
+  }
+
+  return res.status(400).json({ error: 'Could not fetch track' })
 }
 
 module.exports = { httpGetCurrentTrack }
