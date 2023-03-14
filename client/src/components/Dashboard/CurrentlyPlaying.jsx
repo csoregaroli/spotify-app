ddabd52127939d81965dc3554a21eed5ee55fc13
Cameron Soregaroli <csoregaroli@gmail.com>
2023-03-14 07:40:29 -0400

    added antd

--- client/src/components/Dashboard/CurrentlyPlaying.jsx
+++ client/src/components/Dashboard/CurrentlyPlaying.jsx
@@ -1,8 +1,4 @@
-import { useCurrentlyPlaying } from '../../hooks/useCurrentlyPlaying'
-
 export const CurrentlyPlaying = () => {
-  const { currentlyPlaying } = useCurrentlyPlaying()
-
   //   console.log(currentlyPlaying)
 
   return <div>currently playing</div>
