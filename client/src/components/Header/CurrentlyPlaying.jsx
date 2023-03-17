5934beaa6d6b00bd79dda4eb501157d25264591c
Cameron Soregaroli <csoregaroli@gmail.com>
2023-03-16 20:19:24 -0400

    currently playing component skeleton

--- client/src/components/Header/CurrentlyPlaying.jsx
+++ client/src/components/Header/CurrentlyPlaying.jsx
@@ -1,9 +1,31 @@
-import { Card } from 'antd'
+import { Card, Avatar, Typography } from 'antd'
+
+const currentTrack = {
+  trackName: 'Da Fonk (feat. Joni)',
+  artists: ['Mochakk', 'Joni'],
+  imageUrl: '',
+  isPlaying: true,
+}
+
+const { trackName, artists, imageUrl, isPlaying } = currentTrack
+const artistsString = artists.join(', ')
 
 const CurrentlyPlaying = () => {
   //   console.log(currentlyPlaying)
 
-  return <div>currently playing</div>
+  return (
+    <Card size='small' style={{ minWidth: '256px' }}>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <Avatar shape='square' src={imageUrl} style={{ marginRight: '8px' }} />
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <Typography.Text>{trackName}</Typography.Text>
+          <Typography.Text type='secondary' style={{ fontSize: '10px' }}>
+            {artistsString}
+          </Typography.Text>
+        </div>
+      </div>
+    </Card>
+  )
 }
 
 export default CurrentlyPlaying
