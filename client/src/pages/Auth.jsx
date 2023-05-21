90545f004887240c54edfcdbd5e9f595120e9aa7
Cameron Soregaroli <csoregaroli@gmail.com>
2023-05-20 20:38:44 -0400

    sign in page v1

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -3,11 +3,15 @@ import { useLocation } from 'react-router-dom'
 import { SIGNUP, SIGNIN } from '../constants/routes'
 
 import AuthPrompt from '../components/Auth/AuthPrompt'
+import { Image } from 'antd'
 
 export const Auth = () => {
   const [copy, setCopy] = useState()
   const location = useLocation()
   const path = location.pathname
+
+  const imageSrc =
+    'https://images.unsplash.com/photo-1633329102202-eaa697179563?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3213&q=80'
 
   const signUpCopy = {
     header: 'Create a free account',
@@ -30,8 +34,20 @@ export const Auth = () => {
   }, [path])
 
   return (
-    <div>
-      <AuthPrompt copy={copy} />
+    <div style={{ display: 'flex', height: '100vh' }}>
+      <div style={{ width: '50%' }}>
+        <AuthPrompt copy={copy} />
+      </div>
+      <div
+        style={{
+          width: '50%',
+          backgroundImage: `url('https://images.unsplash.com/photo-1633329102202-eaa697179563?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3213&q=80')`,
+          backgroundSize: 'cover',
+          borderRadius: '30px',
+        }}
+      />
+      {/* <Image src={imageSrc} preview={false} height='100%' /> */}
+      {/* </div> */}
     </div>
   )
 }
