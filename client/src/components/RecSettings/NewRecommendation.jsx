54103145130972842d801f4e75bb92d204469d37
Cameron Soregaroli <csoregaroli@gmail.com>
2023-05-22 22:58:09 -0400

    got notifications working

--- client/src/components/RecSettings/NewRecommendation.jsx
+++ client/src/components/RecSettings/NewRecommendation.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { Button, Modal } from 'antd'
+import { useEffect, useState } from 'react'
+import { Button, Modal, notification } from 'antd'
 
 import SelectGenres from '../SelectGenres'
 import RecSlider from './RecSlider'
@@ -16,22 +16,54 @@ const customizations = [
 const modalTitle = 'Generate new recommendations'
 
 const NewRecommendation = ({ isOpen, onClose }) => {
+  //   const [api, contextHolder] = notification.useNotification()
+  const [displayNotification, setDisplayNotification] = useState(null)
   const [selectedGenre, setSelectedGenre] = useState()
   const [sliderValues, setSliderValues] = useState()
   const [confirmLoading, setConfirmLoading] = useState(false)
+
+  const successNotification = () => {
+    notification.success({
+      message: 'New recommendations generated!',
+      placement: 'top',
+    })
+  }
+
+  const errorNotification = () => {
+    notification.error({
+      message: 'Oops something went wrong.',
+      description: 'Please try again.',
+      placement: 'top',
+    })
+  }
 
   const saveSliderValue = (title, value) => {
     setSliderValues({ ...sliderValues, [title]: value })
   }
 
+  useEffect(() => {
+    if (displayNotification) {
+      if (displayNotification.type === 'success') {
+        successNotification()
+      } else if (displayNotification.type === 'error') {
+        errorNotification()
+      }
+      setDisplayNotification(null)
+    }
+  }, [displayNotification])
+
   const handleSubmit = async () => {
-    // setConfirmLoading(true)
+    setConfirmLoading(true)
     const response = await getRecommendations(selectedGenre, sliderValues)
 
-    console.log(response)
-    // if (response?.status === 200) {
-    //   //   setConfirmLoading(false)
-    // }
+    if (response?.status === 200) {
+      setDisplayNotification({ type: 'success' })
+    } else {
+      setDisplayNotification({ type: 'error' })
+    }
+
+    setConfirmLoading(false)
+    onClose()
   }
 
   return (
